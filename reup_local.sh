8396bb3fe5d08150b95f1975acf07f2eb96cc47c
Devraj Mehta <devm33@gmail.com>
2014-02-20 11:07:08 -0500

    moved around local testing scripts again -- just looking for the best shortnames for efficiency and noone else is using them yet so it doesnt matter

---
+++ reup_local.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Script to stop and then start the local build and serve process
+
+bash down_local.sh;
+bash up_local.sh;
+
---
+++ up_local.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Script to start the local build and serve process
+
+node server.js &
+
+compass watch &
+
+
