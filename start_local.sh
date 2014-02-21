bc808230468f8cf14ecbc134b7323bf187b121a8
Devraj Mehta <devm33@gmail.com>
2014-02-21 00:53:11 -0500

    removed refs to old files

--- start_local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Script to start the local build and serve process
-
-node server.js &
-
-compass watch &
-
-
--- stop_local.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Script to stop the local build and serve process
-
-killall -s SIGINT node
-killall -s SIGINT compass
-
