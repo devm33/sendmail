cb8686392129aa2c23afa8fc155b7bbb29fcef58
Devraj Mehta <devm33@gmail.com>
2014-03-03 20:32:50 -0500

    added redis to stack, required two node modules: redis & connect-redis, moved session mgmt to redis

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  Main server file for sendmail app
 */
 
-/*** application settings */
+/*** Application settings */
 var port = process.env.PORT || 5000; // heroku port || local test port
 var testing = (port == 5000); // the port tells us if we're testing
 var host = 'https://sendmail4911.herokuapp.com';
@@ -10,39 +10,50 @@ if(testing) {
     host = 'http://localhost:5000';
 }
 
-/*** required modules */
+/*** Required modules */
 var express = require('express');
 var request = require('request');
+var RedisStore = require('connect-redis')(express);
 var secrets = require('./secrets.json');   /* keys, codes, etc. */
 var auth    = require('./lib/auth.js')(host);/* lib for auth routes */
 var user    = require('./lib/user.js');    /* user routes, api calls */
 var mail    = require('./lib/mail.js');    /* mail handling methods */
 var error   = require('./lib/error.js');   /* error handling methods */
 
-/*** configure application */
+/*** Establish database connection */
+var rtg, redis, redis_opts;
+if (process.env.REDISTOGO_URL) {
+    rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.client.auth(rtg.auth.split(":")[1]);
+} else {
+    redis = require("redis").createClient();
+}
+
+/*** Configure express application */
 var app = express();
-app.configure(function(){
-    // turn on default logging
-    app.use(express.logger());
+// turn on default logging
+app.use(express.logger());
 
-    // use ejs for templates
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
+// use ejs for templates
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-    // enable gzipping
-    app.use(express.compress());
+// enable gzipping
+app.use(express.compress());
 
-    // serve files out of static dir
-    app.use(express.static(__dirname + '/static', {maxAge: 31557600000}));
+// serve files out of static dir
+app.use(express.static(__dirname + '/static', {maxAge: 31557600000}));
 
-    // cookie processing for session
-    app.use(express.cookieParser(secrets.cookie_pass || 'dumb pass'));
+// cookie processing for session
+app.use(express.cookieParser(secrets.cookie_pass || 'dumb pass'));
 
-    // use express's session handling for most of user management
-    app.use(express.session());
-});
+// use express's session handling for most of user management
+app.use(express.session({
+    store: new RedisStore({client: redis})
+}));
 
-/*** endpoints */
+/*** Endpoints */
 app.get('/', function(req, res){
     var template = 'landing';
     var opts = {'gauth_url': auth.gauth_url};
@@ -60,10 +71,10 @@ app.get('/profile', user.profile);
 app.get('/logout', auth.logout);
 app.get(auth.authroute, auth.authorize);
 
-/*** add error handlers */
+/*** Add error handlers */
 require('./lib/error.js')(app);
 
-/*** start server */
+/*** Start server */
 app.listen(port, function() {
     console.log('Listening on ' + port);
 });
