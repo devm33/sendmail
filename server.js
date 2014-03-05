6b0bdccf7f48031609fa248c32b4016f48400a94
Devraj Mehta <devm33@gmail.com>
2014-03-05 01:55:31 -0500

    foolish: redis.client ! redis obj was require('redis').client

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var rtg, redis, redis_opts;
 if (process.env.REDISTOGO_URL) {
     rtg   = require("url").parse(process.env.REDISTOGO_URL);
     redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.client.auth(rtg.auth.split(":")[1]);
+    redis.auth(rtg.auth.split(":")[1]);
 } else {
     redis = require("redis").createClient();
 }
