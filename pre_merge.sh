e201ecd9a0cc131a7fa8a7f37fae57528aef4dd5
Devraj Mehta <devm33@gmail.com>
2014-03-27 22:29:33 -0400

    console.log search added to pre_merge script

--- pre_merge.sh
+++ pre_merge.sh
@@ -11,4 +11,12 @@ compass compile -e production --force
 # jshint the js files (complain if it is not available)
 command -v jshint >/dev/null 2>&1 || { echo >&2 "Please install jshint: npm install -g jshint"; exit 1;}
 
-jshint *.js lib/*.js static/*.js # note: avoid hinting all the node modules
+JSFILES="*.js lib/*.js static/*.js"
+
+if grep -q 'console.log' $JSFILES
+then
+    echo "Warning: console.log found in js"
+    grep 'console.log' $JSFILES
+fi
+
+jshint $JSFILES # note: avoid hinting all the node modules
