d8296afcbc6af1538239ada434e1ac25b3e2f640
David Kearns <davidkearns@users.noreply.github.com>
2014-04-05 01:24:24 -0400

    rough delayed send functionality. changes to config file structure and gmail injection scripts

---
+++ extension/config.js
@@ -0,0 +1,14 @@
+/*Production
+var config = {
+"url":"https://sendmail4911.herokuapp.com/extensionauth"
+};*/
+/*Development*/
+var config = {
+    /*Development*/
+    "url":"http://localhost:5000",
+    /*Production
+    "url":"https://sendmail4911.herokuapp.com",
+    */
+    "extensionauth": "/extensionauth",
+    "schedule": "/schedule"
+};
