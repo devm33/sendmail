ecd3d550172b7a301a30fddfb56864710c5f6a4b
David Kearns <davidkearns@users.noreply.github.com>
2014-04-06 15:30:10 -0400

    fixed error with scheduling success message box selection and also switched to production config in preparation for a pull req and deployment

--- extension/config.js
+++ extension/config.js
@@ -1,8 +1,8 @@
 var config = {
-    /*Development*/
-    "url":"http://localhost:5000",
-    /*Production
-    "url":"https://sendmail4911.herokuapp.com",*/
+    /*Development
+    "url":"http://localhost:5000",*/
+    /*Production*/
+    "url":"https://sendmail4911.herokuapp.com",
     "extensionauth": "/extensionauth",
     "schedule": "/schedule"
 };
