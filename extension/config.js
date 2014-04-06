b62a550fa20dc92b8790b51ce57c14505b0455ae
davidkearns <dave32392@gmail.com>
2014-04-06 15:09:30 -0400

    fixed auth and scheduling so that it will use key field if supplied

--- extension/config.js
+++ extension/config.js
@@ -1,14 +1,8 @@
-/*Production
-var config = {
-"url":"https://sendmail4911.herokuapp.com/extensionauth"
-};*/
-/*Development*/
 var config = {
     /*Development*/
     "url":"http://localhost:5000",
     /*Production
-    "url":"https://sendmail4911.herokuapp.com",
-    */
+    "url":"https://sendmail4911.herokuapp.com",*/
     "extensionauth": "/extensionauth",
     "schedule": "/schedule"
 };
