af56e2226dee16d97f219e00221c70580f55cb72
David Kearns <dave.p.kearns@gmail.com>
2014-04-17 21:55:36 -0400

    added TODO to pretty up help tooltip. also ran pre-merge because i forgot last commit

--- static/custom-tooltips.js
+++ static/custom-tooltips.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
                 return $(this).attr("title");
             }
             else if($(this).attr("id") == "Help"){
+            //TODO clean up this appearance with some mad CSS skillz
                 return '<ul class="list"><li>"Sign in with Google" will take you to the main page of the website.</li><li>After you sign in, you will be presented with a standard form in which you can compose an email message to be sent at a later time.</li><li>At the top of the page, you will also see a toggle button to change between the "Compose" screen and "Scheduled" screen.</li><li>The "Scheduled" screen presents you with a list of all scheduled events</li><li>You can click "edit" or "delete" to change any scheduled items.</li></ul>';
             }
         }
